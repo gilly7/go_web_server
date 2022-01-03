3317a1bd6e1ea5dad9cf74d3be66cf226fc2e517
Gilbert Ngeno <ngenogilbert07@gmail.com>
2022-01-03 18:49:57 +0300

    Accept a form submission POST request

--- server.go
+++ server.go
@@ -26,9 +26,22 @@ func main() {
 	http.Handle("/", fileServer)
 
 	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/form", formHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
+}
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
 }
